37325e91575d21cfca8173aaeb30c25d9fbba5fd
Cauan Friaes <azevedocauan77@gmail.com>
2026-01-07 21:21:54 +0000

    primeiro commit

---
+++ TesteSpring/src/main/java/dev/Friaes/TesteSpring/RecomecandoApplication.java
@@ -0,0 +1,13 @@
+package dev.Friaes.TesteSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecomecandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecomecandoApplication.class, args);
+	}
+
+}
---
+++ TesteSpring/src/test/java/dev/Friaes/TesteSpring/RecomecandoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.Friaes.TesteSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecomecandoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
